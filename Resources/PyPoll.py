095f5b500fbfbea823622a3cbc77a365830fa32f
Koiyan Mawolo <kmawolo@outlook.com>
2021-04-17 23:58:22 -0400

    Adding Pypoll.py file

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -123,3 +123,43 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+
+# # Import the datetime class from the datetime module.
+# import datetime as dt
+# # Use the now() attribute on the datetime class to get the present time.
+# now = dt.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
+
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+# # Using the open() function with the "w" mode we will write data to the file.
+# open(file_to_save, "w")
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Using the with statement open the file as a text file.
+# outfile = open(file_to_save, "w")
+# # Write some data to the file.
+# outfile.write("Hello World")
+
+# # Close the file
+# outfile.close()
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
